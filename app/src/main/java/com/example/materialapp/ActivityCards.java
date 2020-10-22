5729c42c06d339fb9c7e7f3e56acf222a5242185
Uriel <urielenrique99@gmail.com>
2020-10-22 16:54:06 -0500

    Se agrego el bottomAppBar

--- app/src/main/java/com/example/materialapp/ActivityCards.java
+++ app/src/main/java/com/example/materialapp/ActivityCards.java
@@ -2,7 +2,10 @@ package com.example.materialapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ActivityCards extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class ActivityCards extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cards);
+
+        Button btnSig = findViewById(R.id.btnSiguiente);
+
+        btnSig.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), ActivityBottomBar.class);
+                startActivity(intent);
+            }
+        });
     }
 }
